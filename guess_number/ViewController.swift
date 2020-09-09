a4c26a004182dae39d1e4fe813080a70e90a16f2
Pin Huang <justmailmason@gmail.com>
2020-09-09 15:39:23 +0800

    Initial Commit

---
+++ guess_number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  guess_number
+//
+//  Created by Pincheng Huang on 2020/9/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
